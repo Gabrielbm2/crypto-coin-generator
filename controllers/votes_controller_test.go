d4d47e300633a3651ffa64a7a108742eba629a37
Gabriel Bernardo Meireles <gbmreistom@gmail.com>
2023-05-09 00:14:44 -0300

    Votes test

---
+++ controllers/votes_controller_test.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func TestLike(t *testing.T) {
+	req := httptest.NewRequest("POST", "/crypto/1/vote/like", nil)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("cryptoID", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	w := httptest.NewRecorder()
+	Like(w, req)
+
+	if w.Code != http.StatusCreated {
+		t.Errorf("Status code incorreto. Esperado: %d. Obtido: %d", http.StatusCreated, w.Code)
+	}
+}
+
+func TestDislike(t *testing.T) {
+	req := httptest.NewRequest("POST", "/crypto/1/dislike", nil)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("cryptoID", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	w := httptest.NewRecorder()
+	Dislike(w, req)
+
+	if w.Code != http.StatusCreated {
+		t.Errorf("Status code incorreto. Esperado: %d. Obtido: %d", http.StatusCreated, w.Code)
+	}
+}
+
+func TestGetVotes(t *testing.T) {
+	req := httptest.NewRequest("GET", "/votes/1", nil)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("cryptoID", "1")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	w := httptest.NewRecorder()
+	GetVotes(w, req)
+
+	expectedResponse := `{"id":"1","name":"Bitcoin","votes":10}`
+	if w.Body.String() != expectedResponse {
+		t.Errorf("Resposta incorreta. Esperado: %s. Obtido: %s", expectedResponse, w.Body.String())
+	}
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Status code incorreto. Esperado: %d. Obtido: %d", http.StatusOK, w.Code)
+	}
+}
