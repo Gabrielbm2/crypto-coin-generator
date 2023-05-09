a643dd080eed9024f1ae3b9117507d86e034c461
Gabriel Bernardo Meireles <gbmreistom@gmail.com>
2023-05-09 00:06:47 -0300

    Crypto coins test

--- controllers/crypto_coins_controller_test.go
+++ controllers/crypto_coins_controller_test.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"desafioKlever/models"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,7 +23,47 @@ func TestGetCrypto(t *testing.T) {
 	w := httptest.NewRecorder()
 	GetCrypto(w, req)
 
-	expectedResponse := `{"id":"1","name":"Bitcoin"}`
+	expectedResponse := `{"id":"1","name":"Klever"}`
+	if w.Body.String() != expectedResponse {
+		t.Errorf("Resposta incorreta. Esperado: %s. Obtido: %s", expectedResponse, w.Body.String())
+	}
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Status code incorreto. Esperado: %d. Obtido: %d", http.StatusOK, w.Code)
+	}
+}
+
+func TestCreateCrypto(t *testing.T) {
+	// Cria uma nova criptomoeda
+	crypto := models.CreateCryptoPayload{
+		ID:   "1",
+		Name: "Klever",
+	}
+
+	// Codifica a criptomoeda em JSON
+	jsonCrypto, _ := json.Marshal(crypto)
+
+	// Cria um reader com os dados codificados em JSON
+	reader := bytes.NewReader(jsonCrypto)
+
+	// Cria um novo request com o reader como corpo
+	req := httptest.NewRequest("POST", "/crypto", reader)
+
+	w := httptest.NewRecorder()
+	CreateCrypto(w, req)
+
+	if w.Code != http.StatusCreated {
+		t.Errorf("Status code incorreto. Esperado: %d. Obtido: %d", http.StatusCreated, w.Code)
+	}
+}
+
+func TestGetCryptos(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cryptos", nil)
+	w := httptest.NewRecorder()
+
+	GetCryptos(w, req)
+
+	expectedResponse := `[{"id":"1","name":"Bitcoin"},{"id":"2","name":"Ethereum"},{"id":"3","name":"Binance Coin"}]`
 	if w.Body.String() != expectedResponse {
 		t.Errorf("Resposta incorreta. Esperado: %s. Obtido: %s", expectedResponse, w.Body.String())
 	}
